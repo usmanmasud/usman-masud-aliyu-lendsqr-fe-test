a209e69eb3c751e8782f76b1e996ada0e5e0a3bb
usmanmasud <168911969+usmanmasud@users.noreply.github.com>
2026-01-24 22:02:06 +0100

    Update users.service.ts

--- services/users.service.ts
+++ services/users.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { User } from "@/types/user";
 
-const API_URL = "";
+const API_URL = "https://mocki.io/v1/f3863e42-8d2e-4483-924c-a780fca3a158";
 
 export const fetchUsers = async (): Promise<User[]> => {
   const res = await axios.get(API_URL);
